6e0f377d9a485c52645711ae57231eeebf374365
Jacob Britton <jacobbritton95@gmail.com>
2023-09-25 20:48:25 -0400

    Add gitignore

--- modules/present/conjugate-present-regular-re.test.js
+++ modules/present/conjugate-present-regular-re.test.js
@@ -1,6 +1,6 @@
 import { conjugatePresentRegularEr } from './conjugate-present-regular-er.js'
 
-describe('Regular re verbs 1st person', () => {
+describe('Conjugating regular re verbs', () => {
     test('conjugating parler in the 1st person returns parle', () => {
         expect(conjugatePresentRegularEr('parler', 'singular-1st')).toBe('parle');
     });
