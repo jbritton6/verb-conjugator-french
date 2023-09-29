5297f835c7363c4bf8893c082bdd7f06e0037355
Jacob Britton <jacobbritton95@gmail.com>
2023-09-29 13:50:46 -0400

    Add extra unit tests

--- modules/present/groups/present-irregular-like-appeler.test.js
+++ modules/present/groups/present-irregular-like-appeler.test.js
@@ -20,4 +20,44 @@ describe('Conjugating present tense irregular ouvrir like verbs', () => {
     test('3rd person of appeler plural returns appellent', () => {
         expect(presentIrregularLikeAppeler('appeler', 'plural-3rd')).toBe('appellent');
     });
+
+
+    test('1st person of épeler singular returns épelle', () => {
+        expect(presentIrregularLikeAppeler('épeler', 'singular-1st')).toBe('épelle');
+    });
+    test('2nd person of épeler singular returns épelles', () => {
+        expect(presentIrregularLikeAppeler('épeler', 'singular-2nd')).toBe('épelles');
+    });
+    test('3rd person of épeler singular returns épelle', () => {
+        expect(presentIrregularLikeAppeler('épeler', 'singular-3rd')).toBe('épelle');
+    });
+    test('1st person of épeler plural returns épelons', () => {
+        expect(presentIrregularLikeAppeler('épeler', 'plural-1st')).toBe('épelons');
+    });
+    test('2nd person of épeler plural returns épelez', () => {
+        expect(presentIrregularLikeAppeler('épeler', 'plural-2nd')).toBe('épelez');
+    });
+    test('3rd person of épeler plural returns épellent', () => {
+        expect(presentIrregularLikeAppeler('épeler', 'plural-3rd')).toBe('épellent');
+    });
+
+
+    test('1st person of hoqueter singular returns hoquette', () => {
+        expect(presentIrregularLikeAppeler('hoqueter', 'singular-1st')).toBe('hoquette');
+    });
+    test('2nd person of hoqueter singular returns hoquettes', () => {
+        expect(presentIrregularLikeAppeler('hoqueter', 'singular-2nd')).toBe('hoquettes');
+    });
+    test('3rd person of hoqueter singular returns hoquette', () => {
+        expect(presentIrregularLikeAppeler('hoqueter', 'singular-3rd')).toBe('hoquette');
+    });
+    test('1st person of hoqueter plural returns hoquetons', () => {
+        expect(presentIrregularLikeAppeler('hoqueter', 'plural-1st')).toBe('hoquetons');
+    });
+    test('2nd person of hoqueter plural returns hoquetez', () => {
+        expect(presentIrregularLikeAppeler('hoqueter', 'plural-2nd')).toBe('hoquetez');
+    });
+    test('3rd person of hoqueter plural returns hoquettent', () => {
+        expect(presentIrregularLikeAppeler('hoqueter', 'plural-3rd')).toBe('hoquettent');
+    });
 })
